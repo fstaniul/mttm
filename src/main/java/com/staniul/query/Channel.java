5212289fb9b44f8cc24555f9e4f43766fb43b9a3
vip1all <vip.all@hotmail.com>
2017-06-10 00:08:45 +0200

    Created channel, container for channel information.

--- src/main/java/com/staniul/query/Channel.java
+++ src/main/java/com/staniul/query/Channel.java
@@ -1,9 +1,105 @@
 package com.staniul.query;
 
+import com.staniul.query.channel.ChannelFlagConstants;
+
+import java.util.Map;
+
 /**
  * Contains information about teamspeak 3 channel
  */
-public class Channel {
+public class Channel implements ChannelFlagConstants {
+    private int id;
+    private int parentId;
+    private int orderId;
+
+    private String name;
+    private String topic;
+
+    private int codec;
+    private int codecQuality;
+
+    private long secondsEmpty;
+
     private int flags;
 
+    Channel(int id, Map<String, String> info) {
+        this.id = id;
+        parentId = Integer.parseInt(info.get("pid"));
+        orderId = Integer.parseInt(info.get("channel_order"));
+
+        name = info.get("channel_name");
+        topic = info.get("channel_topic");
+
+        codec = Integer.parseInt("channel_codec");
+        codecQuality = Integer.parseInt("channel_codec_quality");
+
+        secondsEmpty = Long.parseLong("seconds_empty");
+
+        flags = ChannelFlagConstants.parseFlags(info);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public int getCodec() {
+        return codec;
+    }
+
+    public int getCodecQuality() {
+        return codecQuality;
+    }
+
+    public long getSecondsEmpty() {
+        return secondsEmpty;
+    }
+
+    public boolean isPermanent() {
+        return (flags & PERMANENT) == 1;
+    }
+
+    public boolean isSemiPermanent() {
+        return (flags & SEMI_PERMANENT) == SEMI_PERMANENT;
+    }
+
+    public boolean areMaxClientsUnlimited() {
+        return (flags & MAXCLIENTS_UNLIMITED) == MAXCLIENTS_UNLIMITED;
+    }
+
+    public boolean areFamilyMaxClientsUnlimited() {
+        return (flags & MAXFAMILYCLIENTS_UNLIMITED) == MAXFAMILYCLIENTS_UNLIMITED;
+    }
+
+    public boolean isPasswordProtected() {
+        return (flags & PASSWORD) == PASSWORD;
+    }
+
+    public boolean isDefaultChannel() {
+        return (flags & DEFAULT) == DEFAULT;
+    }
+
+    /**
+     * Sets a flag. If flag was already set it will disable the flag. It just simply does a XOR with flag.
+     *
+     * @param flag One of {@link ChannelFlagConstants}.
+     */
+    public void set(int flag) {
+        flags ^= flag;
+    }
 }
--- src/main/java/com/staniul/query/channel/ChannelFlagConstants.java
+++ src/main/java/com/staniul/query/channel/ChannelFlagConstants.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 /**
  * Represents constant values for channel flags!
- * Channel flag for inherited max family clients is not supported, and is set to 0 in {@link ChannelFlags#toString()}
+ * Channel flag for inherited max family clients is not supported, and is set to 0 in {@link #flagsToString(int)}}
  */
 public interface ChannelFlagConstants {
     /**
@@ -32,7 +32,7 @@ public interface ChannelFlagConstants {
 
     /**
      * Indicates if channel has password or not.
-     * It is not possible to set this property directly and that's why it is omitted in {@link ChannelFlags#toString()} method.
+     * It is not possible to set this property directly and that's why it is omitted in {@link #flagsToString(int)} method.
      */
     int PASSWORD = 1 << 4;
 
@@ -49,4 +49,48 @@ public interface ChannelFlagConstants {
      * will result in flags being set to 0 and that to unwanted behaviour.
      */
     int __DEFAULT_FLAGS = PERMANENT | MAXCLIENTS_UNLIMITED | MAXFAMILYCLIENTS_UNLIMITED;
+
+    /**
+     * Transforms flags stored as {@code int} number into a readable by teamspeak 3 server query set of flags
+     * contained in one string. Used by channel properties for setting flags on new created channel and by Channel for
+     * reading flags.
+     *
+     * @return String containing flags readable by teamspeak 3 query.
+     */
+    static String flagsToString (int flags) {
+        return "channel_flag_permanent=" + (flags & PERMANENT) + " " +
+                "channel_flag_semi_permanent=" + (flags & SEMI_PERMANENT) / SEMI_PERMANENT + " " +
+                "channel_flag_maxclients_unlimited=" + (flags & MAXCLIENTS_UNLIMITED) / MAXCLIENTS_UNLIMITED + " " +
+                "channel_flag_maxfamilyclients_unlimited=" + (flags & MAXFAMILYCLIENTS_UNLIMITED) / MAXFAMILYCLIENTS_UNLIMITED + " " +
+                "channel_flag_maxfamilyclients_inherited=0 " +
+                "channel_flag_default=" + (flags & DEFAULT) / DEFAULT;
+    }
+
+    /**
+     * <p>Reads flags from map containing channel information. This method should be used only by {@link
+     * com.staniul.query.Query Query} and {@link com.staniul.query.Channel} classes to parse teamspeak 3 server query
+     * information.</p>
+     * <p>If you want to get channel flags and/or information go see {@link
+     * com.staniul.query.Query#getChannelInfo(int)}</p>
+     *
+     * @param channelInfo Information about channel read from teamspeak 3 query contained in map.
+     *
+     * @return {@code int} that represents flags of teamspeak 3 channel.
+     */
+    static int parseFlags(Map<String, String> channelInfo) {
+        int flags = 0;
+        flags |= Integer.parseInt(channelInfo.get("channel_flag_permanent")) * PERMANENT;
+        flags |= Integer.parseInt(channelInfo.get("channel_flag_semi_permanent")) * SEMI_PERMANENT;
+
+        int maxclients = Integer.parseInt(channelInfo.get("channel_maxclients"));
+        flags |= maxclients == -1 ? MAXCLIENTS_UNLIMITED : 0;
+
+        int maxfamilyclients = Integer.parseInt(channelInfo.get("channel_maxfamilyclients"));
+        flags |= maxfamilyclients == -1 ? MAXFAMILYCLIENTS_UNLIMITED : 0;
+
+        flags |= Integer.parseInt(channelInfo.get("channel_flag_password")) * PASSWORD;
+        flags |= Integer.parseInt(channelInfo.get("channel_flag_default")) * DEFAULT;
+
+        return flags;
+    }
 }
