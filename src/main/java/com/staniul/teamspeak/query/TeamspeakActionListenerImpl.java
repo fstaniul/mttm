03e006a27f859d7c0062aa9883debb672b9f5fd9
vip1all <vip.all@hotmail.com>
2017-06-18 22:42:39 +0200

    Updated admin welcome message and message on admin joining a specified channel.

--- src/main/java/com/staniul/teamspeak/query/TeamspeakActionListenerImpl.java
+++ src/main/java/com/staniul/teamspeak/query/TeamspeakActionListenerImpl.java
@@ -1,6 +1,7 @@
 package com.staniul.teamspeak.query;
 
 import com.staniul.teamspeak.TeamspeakCoreController;
+import com.staniul.util.collections.SetUtil;
 import com.staniul.xmlconfig.ConfigurationLoader;
 import com.staniul.xmlconfig.CustomXMLConfiguration;
 import com.staniul.xmlconfig.annotations.UseConfig;
@@ -8,7 +9,10 @@ import de.stefan1200.jts3serverquery.TeamspeakActionListener;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.log4j.Logger;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @UseConfig("query.xml")
 public class TeamspeakActionListenerImpl implements TeamspeakActionListener {
@@ -31,6 +35,16 @@ public class TeamspeakActionListenerImpl implements TeamspeakActionListener {
         }
 
         else if ("notifycliententerview".equals(eventType) && !"serveradmin".equals("client_unique_identifier")) {
+            Set<Integer> servergroups = Arrays.stream(eventInfo.get("client_servergroups").split(",")).map(Integer::parseInt).collect(Collectors.toSet());
+            Set<Integer> admingroups = config.getIntSet("welcome[@groups]");
+            if (SetUtil.countIntersection(servergroups, admingroups) > 0L) {
+                try {
+                    String msg = config.getString("welcome[@msg]").replace("$NICKNAME$", eventInfo.get("client_nickname"));
+                    query.sendTextMessageToClient(Integer.parseInt(eventInfo.get("clid")), msg);
+                } catch (QueryException e) {
+                    log.error("Failed to send welcome message to admin that joined server.");
+                }
+            }
             invokeJoinEvent(eventInfo);
         }
 
@@ -41,7 +55,9 @@ public class TeamspeakActionListenerImpl implements TeamspeakActionListener {
         else if ("notifyclientmoved".equals(eventType) &&
                 eventInfo.get("ctid").equals(config.getString("event-channel[@id]"))) {
             try {
-                query.sendTextMessageToClient(Integer.parseInt(eventInfo.get("clid")), config.getString("welcome-msg"));
+                Client client = query.getClientInfo(Integer.parseInt(eventInfo.get("clid")));
+                String msg = config.getString("welcome[@msg]").replace("$NICKNAME$", client.getNickname());
+                query.sendTextMessageToClient(client.getId(), msg);
             } catch (QueryException e) {
                 log.error("Failed to send message to admin when he joined channel to get message from bot!", e);
             }
