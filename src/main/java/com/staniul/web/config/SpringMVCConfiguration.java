ed12661f471342e9a191c133430d4823f3d450bd
vip1all <vip.all@hotmail.com>
2017-07-19 13:51:11 +0200

    Edited start/stop script.

--- src/main/java/com/staniul/web/config/SpringMVCConfiguration.java
+++ src/main/java/com/staniul/web/config/SpringMVCConfiguration.java
@@ -1,12 +1,12 @@
 package com.staniul.web.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
-@EnableWebSecurity
-public class SpringMVCConfiguration {
+//@EnableWebMvc
+//@EnableWebSecurity
+public class SpringMVCConfiguration  {
 
 }
