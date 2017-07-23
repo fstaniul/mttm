e7e1a5f909375af2bfd45783a05c540c7c56fe2e
vip1all <vip.all@hotmail.com>
2017-07-23 17:51:49 +0200

    Preset for working with database ready.

--- src/main/java/com/staniul/MTTMSpringConfiguration.java
+++ src/main/java/com/staniul/MTTMSpringConfiguration.java
@@ -8,7 +8,13 @@ import org.reflections.scanners.SubTypesScanner;
 import org.reflections.scanners.TypeAnnotationsScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * Configuration class for Spring
@@ -25,5 +31,23 @@ public class MTTMSpringConfiguration {
                 .setScanners(new SubTypesScanner(),
                         new MethodAnnotationsScanner(),
                         new TypeAnnotationsScanner()));
+    }
+
+    @Bean
+    @Primary
+    public DataSource teamspeakDatabase (@Value("db.username") String username, @Value("db.password") String password) {
+        DriverManagerDataSource source = new DriverManagerDataSource("jdbc:mysql://localhost:3306/teamspeak", username, password);
+        source.setDriverClassName("com.mysql.jdbc.Driver");
+        return source;
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate (DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate (DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
