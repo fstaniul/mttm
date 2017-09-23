c6bfb16e40bb83014e940ae46236dbed6251a34f
vip1all <vip.all@hotmail.com>
2017-09-24 00:36:42 +0200

    Fixed errors in RmiController

--- src/main/java/com/staniul/teamspeak/rmi/RmiControllerImpl.java
+++ src/main/java/com/staniul/teamspeak/rmi/RmiControllerImpl.java
@@ -1,29 +1,32 @@
 package com.staniul.teamspeak.rmi;
 
+import org.springframework.stereotype.Component;
+
+import java.rmi.RemoteException;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-//@Component
+@Component
 public class RmiControllerImpl implements RmiController {
     @Override
-    public <R> R supply (Supplier<R> supplier) {
+    public <R> R supply (Supplier<R> supplier) throws RemoteException {
         return supplier.get();
     }
 
     @Override
-    public <T> void consume(T t, Consumer<T> consumer) {
+    public <T> void consume(T t, Consumer<T> consumer) throws RemoteException {
         consumer.accept(t);
     }
 
     @Override
-    public <T, R> R doAction(T t, Function<T, R> action) {
+    public <T, R> R doAction(T t, Function<T, R> action) throws RemoteException {
         return action.apply(t);
     }
 
     @Override
-    public <T, U, R> R doAction (T t, U u, BiFunction<T, U, R> action) {
+    public <T, U, R> R doAction (T t, U u, BiFunction<T, U, R> action) throws RemoteException {
         return action.apply(t, u);
     }
 }
