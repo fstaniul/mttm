965041241b905094e7ff704c69c1d6dc501e5ba2
Filip <vip1all@users.noreply.github.com>
2017-09-24 04:01:54 +0200

    Update RmiController.java

--- src/main/java/com/staniul/rmi/RmiController.java
+++ src/main/java/com/staniul/rmi/RmiController.java
@@ -10,7 +10,7 @@ import java.util.function.Supplier;
 public interface RmiController extends Remote {
     <R> R supply(Supplier<R> supplier) throws RemoteException;
     <T> void consume (T t, Consumer<T> consumer) throws RemoteException;
-    <T, R> R doAction (T t, Function<T, R> action) throws RemoteException;
+    <T extends Serializable, R extends Serializable> R doAction (T t, SerFunction<T, R> action) throws RemoteException;
     <T, U, R> R doAction(T t, U u, BiFunction<T, U, R> action) throws RemoteException;
     public String echo(String str) throws  RemoteException;
 }
