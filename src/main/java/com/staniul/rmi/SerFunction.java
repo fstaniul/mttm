fa2a110e7455d6acf49ef810e6d0d3e77250e21f
vip1all <vip.all@hotmail.com>
2017-10-02 15:26:11 +0200

    Deleted not working rmi which should be rewritten.

--- src/main/java/com/staniul/rmi/SerFunction.java
+++
@@ -1,9 +0,0 @@
-package com.staniul.rmi;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.util.function.Function;
-
-interface SerFunction<T, R> extends Function<T, R>, Serializable { }
